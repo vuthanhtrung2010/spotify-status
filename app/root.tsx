719a3d73013f165d192ee12217f85693d72c4834
default <vuthanhtrungsuper@gmail.com>
2024-05-19 11:27:45 +0000

    meta func

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, MetaFunction } from "@remix-run/react";
 import { LinksFunction } from "@remix-run/node";
 import appStylesHref from "./styles.css?url";
 
@@ -6,36 +6,37 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Trung's Spotify Status" },
+    {
+      property: "og:title",
+      content: "A website which displays my spotify status.",
+    },
+    {
+      name: "description",
+      content: "A website which displays my spotify status.",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+      type: "image/x-icon",
+    },
+    {
+      property: "og:image",
+      content: "/assets/banner.png",
+    },
+  ];
+};
+
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <title>Trung's Spotify Status</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* HTML Meta Tags */}
-        <meta name="description" content="A website which displays my spotify status." />
-        <meta name="theme-color" content="#0096ff" />
-
-        {/* Google / Search Engine Tags */}
-        <meta itemProp="name" content="Trung" />
-        <meta itemProp="description" content="A website which displays my spotify status." />
-        <meta itemProp="image" content="/assets/banner.png" />
-
-        {/* Facebook Meta Tags */}
-        <meta property="og:url" content="https://spotify.trung.is-a.dev" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Trung" />
-        <meta property="og:description" content="A website which displays my spotify status." />
-        <meta property="og:image" content="/assets/banner.png" />
-
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Trung" />
-        <meta name="twitter:description" content="A website which displays my spotify status." />
-        <meta name="twitter:image" content="/assets/banner.png" />
-
-        <link rel="icon" href="/favicon.ico"></link>
         <Meta />
         <Links />
       </head>
