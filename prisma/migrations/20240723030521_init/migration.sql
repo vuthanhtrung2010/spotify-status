2d723f4255db5e8b2a761fa4a85901707f6bc26c
Restyled.io <commits@restyled.io>
2024-08-09 04:03:56 +0000

    Restyled by pg_format

--- prisma/migrations/20240723030521_init/migration.sql
+++ prisma/migrations/20240723030521_init/migration.sql
@@ -1,18 +1,18 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "token" TEXT,
-    "refreshToken" TEXT,
-
+    "id" serial NOT NULL,
+    "email" text NOT NULL,
+    "token" text,
+    "refreshToken" text,
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_email_key" ON "User" ("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_token_key" ON "User"("token");
+CREATE UNIQUE INDEX "User_token_key" ON "User" ("token");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_refreshToken_key" ON "User"("refreshToken");
+CREATE UNIQUE INDEX "User_refreshToken_key" ON "User" ("refreshToken");
+
