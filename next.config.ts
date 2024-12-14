8c6782e683ac01a0c9406f81c15ec020f51f57cf
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-12-14 11:36:15 +0000

    Delete source mapping after build

--- next.config.ts
+++ next.config.ts
@@ -54,9 +54,9 @@ export default withSentryConfig(nextConfig, {
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
   tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
