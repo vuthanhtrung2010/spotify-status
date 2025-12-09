02fe8d7e05558eddc74e4d62e9e699414ea6be8c
vuthanhtrung2010 <vuthanhtrungsuper@gmail.com>
2025-12-09 11:36:44 +0700

    chore: update deps and adapt new prisma configuration

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import type { PrismaConfig } from "prisma";
+import { env } from "prisma/config";
+
+export default {
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: { 
+    url: env("DATABASE_URL") 
+  }
+} satisfies PrismaConfig;
